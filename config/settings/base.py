d1ed15de138e0aaef0ab324815b7d61462163d96
seungwon0113 <dhkd0113@naver.com>
2024-11-01 21:54:15 +0900

    🐛fix : 기능 수정, 버그 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,22 +10,53 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import json
+import os
+from datetime import timedelta
 from pathlib import Path
+from django.core.mail.backends.smtp import EmailBackend
+from rest_framework.settings import api_settings
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
+secrets_file_path = os.path.join(BASE_DIR, ".config_secret/secrets.json")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-im8@bpwvi$@so!+7w8cp6npw^se*s#5tfy#q7xz2morb0t5dhl"
+if os.path.exists(secrets_file_path):
+    with open(secrets_file_path) as f:
+        SECRET = json.load(f)
+else:
+    raise FileNotFoundError(f"Secrets file not found at {secrets_file_path}")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
+
+# REST Framework 설정에 JWT 인증 설정 추가
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
+}
+
+# Simple JWT 설정
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),  # 토큰의 유효 시간 설정
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_COOKIE": "access_token",  # 액세스 토큰을 쿠키에 저장
+    "AUTH_COOKIE_REFRESH": "refresh_token",  # 리프레시 토큰을 쿠키에 저장
+    "AUTH_COOKIE_SECURE": True,  # https 환경에서 쿠키 보안 설정
+    "AUTH_COOKIE_HTTP_ONLY": True,  # 쿠키를 HTTP 전용으로 설정해 자바스크립트로 접근 불가
+    "AUTH_COOKIE_PATH": "/",
+    "AUTH_COOKIE_SAMESITE": "Lax",
+}
 
 
 # Application definition
@@ -37,6 +68,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # REST_FRAMEWORK
+    "drf_spectacular",
+    "rest_framework",
+    "psycopg2",
+    # THIRD_APPS
+    "users",
+    "accounts",
+    "transaction_history",
 ]
 
 MIDDLEWARE = [
@@ -75,11 +115,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "gagyebu",
+        "USER": "mac",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
