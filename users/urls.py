187aee15764928218242c15a96b9abbc4c209cc2
seungwon0113 <dhkd0113@naver.com>
2024-11-04 18:43:03 +0900

    🐛fix : 기능 수정, - production ROOT_URLCOF added

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,7 @@
 # users/urls.py
 from django.urls import path
 
-from .views import (
-    # ActivateUserView,
+from .views import (  # ActivateUserView,
     CookieTokenRefreshView,
     UserLoginView,
     UserLogoutView,
